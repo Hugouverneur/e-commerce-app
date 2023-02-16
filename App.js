88cf166424210aabde22787588272f5bf44e62d6
Hugo <hugo.umbert@estiam.com>
2023-02-16 12:53:05 +0100

    Fix load product page in App.js

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ProductAddScreen from './src/screen/Product/ProductAddScreen';
-import ProductScreen from './src/screen/Product/ProductScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductScreen/>
-      {/* <ProductAddScreen/> */}
       <StatusBar style="auto" />
     </View>
   );
