ad1c37fa78bbdc3a8cb7fca5be76f8b705d21d64
Mathieu <mathieu.ranieri@outlook.fr>
2023-02-16 19:48:03 +0100

    Add Header & Categories & List

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Navbar from './src/components/Navbar';
+import Header from './src/components/Header'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Header/>
       <Navbar/>
     </SafeAreaView>
   );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const ListItem = () => {
+const ListItem = (props) => {
   return (
-    <View style={styles.listItem}>
-      <Text>ListItem</Text>
-    </View>
+    <TouchableOpacity style={styles.listItem}>
+      <Image
+        source={{uri:'https://www.neozone.org/blog/wp-content/uploads/2021/05/chat-egypte-001-780x470.jpg'}}
+        style={styles.itemImage}
+      />
+      <View style={styles.itemInfo}>
+        <Text style={styles.itemTitle}>{props.title}</Text>
+        <Text style={styles.itemDate}>Postée le : {props.date}</Text>
+        <Text style={styles.itemDescription}>{props.description}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
     listItem: {
-        
+      flex: 1,
+      flexDirection: 'row',
+      textAlign: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#eef6fc',
+      margin: 19,
+      borderRadius: 10
+    },
+
+    itemTitle: {
+      fontSize: 18,
+      color: '#7d6dff',
+      fontWeight: 'bold'
+    },
+
+    itemImage: {
+      width: 100, 
+      height: 100,
+      margin: 10,
+      borderRadius: 10
+    },
+
+    itemInfo: {
+      width: '50%',
+      margin: 5
+    },
+
+    itemDate: {
+      fontStyle: 'italic',
+      color: '#aaaaaa'
+    },
+
+    itemDescription: {
+      textAlign: 'justify'
     }
 })
 
